02eae3d4679cbb6914456bf532aa0a5ae1c2e240
Michael Chen <643487053@qq.com>
2018-10-25 21:56:48 +0800

    初始化项目

---
+++ src/main/java/com/ntc/concurrent/util/concurrentUtil.java
@@ -0,0 +1,12 @@
+package com.ntc.concurrent.util;
+
+/**
+ * 一些工具类
+ * @author Michael-Chen
+ */
+public class concurrentUtil {
+
+	public static String parentheses(String value) {
+		return "[ "+ value +" ]";
+	}
+}
